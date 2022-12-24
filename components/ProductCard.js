fd06b3548626cedd408e80b8d8c0932381a0e645
sushiroll <jjames@lullipop.com>
2022-12-24 01:04:36 +0000

    helper

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,9 +1,11 @@
 import Link from "next/link"
 import Image from "next/legacy/image"
+import  { formatter } from "../utlis/helpers"
 
 const ProductCard = ({ product }) => {
     const { handle, title } = product.node
     const { altText, url } = product.node.images.edges[0].node
+    const price = product.node.priceRange.minVariantPrice.amount
   
   
     return (
@@ -20,6 +22,7 @@ const ProductCard = ({ product }) => {
                 </div>
             </div>
             <h3 className="mt-4 text-lg font-medium text-gray-900">{title}</h3>
+            <p className="mt-1 text-sm textgray-700">{formatter.format(price)}</p>
         </Link>)
     );
 }
